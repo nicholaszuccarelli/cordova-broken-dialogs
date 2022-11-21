a19afea7769ed67dda1ceb80848ba63398cb196c
Nick Zuccarelli <nicholaszuccarelli@gmail.com>
2022-11-21 11:22:33 +1100

    Cordova dialog bug

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn label="Open Dialog" @click="dialogOpen = true" />
+
+    <q-dialog v-model="dialogOpen">
+      <q-card>
+        <q-card-section>
+          Double tap outside to close it on v1.22.1
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      dialogOpen: false
+    }
+  }
+}
+</script>
